a45f6eb761402f72576baa76127fe8cc8af63294
Noah Loomis <nloomis@spiria.com>
2024-12-20 09:29:55 -0500

    Initial commit

---
+++ lua/configs/lspconfig.lua
@@ -0,0 +1,30 @@
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
+
+local lspconfig = require "lspconfig"
+
+-- EXAMPLE
+local servers = { "html", "cssls", "svelte", "prismals", "biome", "docker_compose_language_service"}
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
+lspconfig.ts_ls.setup {
+  on_attach = nvlsp.on_attach,
+  root_dir = lspconfig.util.root_pattern("package.json"),
+  single_file_support = false
+}
+
+-- configuring single server, example: typescript
+-- lspconfig.ts_ls.setup {
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }
---
+++ lua/options.lua
@@ -0,0 +1,9 @@
+require "nvchad.options"
+
+-- add yours here!
+vim.g.markdown_fenced_languages = {
+  "ts=typescript"
+}
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!

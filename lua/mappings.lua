774d226833d4fb8f2e0cf057e9438075a6e6eff9
Noah Loomis <noah.loomis@me.com>
2025-10-17 02:23:34 +0100

    Re-added lazygit to nvim

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,11 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "Y", 'gg"+yG', { desc = "Copy the whole file to clipboard" })
-map("n", "<leader>ct", "<cmd>Telescope lsp_code_action<cr>") -- normal mode
-map("v", "<leader>ct", "<cmd>Telescope lsp_code_action<cr>") -- visual mode
 
 map({ "n", "v" }, "<leader>ca", function()
   vim.lsp.buf.code_action()
 end, { desc = "LSP Code Actions" })
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -54,8 +54,28 @@ return {
         "typescript",
         "vim",
         "yaml",
-        "svelte"
+        "svelte",
       },
+    },
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
     },
   },
 }
